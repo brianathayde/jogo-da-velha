4c791850f1401c3f4804ba36450f3650e845a167
Brian Athayde <brian_athayde@hotmail.com>
2021-07-02 04:37:06 -0300

    Definindo classe Jogador

---
+++ back-end/Jogador.js
@@ -0,0 +1,9 @@
+class Jogador{
+    constructor(nome, vitorias, derrotas){
+        this.nome = nome;
+        this.vitorias = vitorias;
+        this.derrotas = derrotas;
+    }
+}
+
+exports.Jogador = Jogador
